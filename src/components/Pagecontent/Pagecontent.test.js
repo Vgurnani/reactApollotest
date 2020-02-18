1051e3b559b9574e7558659edb0c52500ef99e10
Vaibhav <vaibhavgstpl@gmail.com>
2020-02-18 17:21:03 +0530

    work on snapshot update test case

--- src/components/Pagecontent/Pagecontent.test.js
+++ src/components/Pagecontent/Pagecontent.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import PageContent from "./index";
 import TestContainer from "../TestContainer";
+import { shallow } from "enzyme";
 
 describe("Test PageContent component", () => {
   it("HTMl elements renders correctly", () => {
@@ -10,7 +11,13 @@ describe("Test PageContent component", () => {
         <PageContent />
       </TestContainer>
     );
+    const wrapper = shallow(
+      <TestContainer>
+        <PageContent />
+      </TestContainer>
+    );
     const linkElement = getByText(/Loading.../i);
     expect(linkElement).toBeInTheDocument();
+    expect(wrapper).toMatchSnapshot();
   });
 });
